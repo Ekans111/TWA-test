555f71ccfeebe9c91bb5c6c458deba9956b259e7
ekans111 <kobayasitorao111@gmail.com>
2024-06-08 06:26:12 +0900

    add icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { TonConnectButton } from "@tonconnect/ui-react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const [ton, setTon] = useState(0);
   return (
     <>
-      <div className='relative flex w-full'>
-        <h1 className='text-3xl text-center'>Telegram Mini App</h1>
-        <div className='text-center absolute right-0 bg-red-500 p-2'>Connect TON Wallet</div>
+      <div className="flex w-full relative">
+        <h1 className="text-3xl text-center w-full">Telegram Mini App</h1>
+        <TonConnectButton className="absolute right-0" />
       </div>
-      <div className='flex justify-center'>
+      <div className="text-2xl mt-10">
+        Token count: {ton}
+      </div>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center rounded-full cursor-pointer transition hover:scale-110 hover:opacity-50 active:opacity-80">
+        <img src="/toncoin.svg" alt="toncoin" className="w-20 h-20" />{" "}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
